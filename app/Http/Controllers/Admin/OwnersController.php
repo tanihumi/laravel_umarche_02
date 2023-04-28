07cac1d0601f390e08a9d7f93ca1282743ae08ff
tanihumi <x7azzsy@gmail.com>
2023-04-28 16:58:58 +0900

    pagenation

--- app/Http/Controllers/Admin/OwnersController.php
+++ app/Http/Controllers/Admin/OwnersController.php
@@ -35,7 +35,7 @@ class OwnersController extends Controller
 
         //dd($e_all, $q_get, $q_first, $c_test);
 
-        $owners = Owner::select('id', 'name', 'email', 'created_at')->get();
+        $owners = Owner::select('id', 'name', 'email', 'created_at')->paginate(3);
         return view('admin.owners.index', compact('owners'));
     }
 
@@ -130,7 +130,7 @@ class OwnersController extends Controller
 
     public function expiredOwnerDestroy($id)
     {
-        Owner::withTrashed()->findOrFail($id)->forceDelete();
+        Owner::onlyTrashed()->findOrFail($id)->forceDelete();
 
         return redirect()->route('admin.expired-owners.index');
     }
