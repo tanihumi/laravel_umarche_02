e86b56b339c68c30880cfeb07d3b2131948be4c6
tanihumi <x7azzsy@gmail.com>
2023-05-02 16:12:26 +0900

    edit update

--- app/Http/Controllers/Owner/ImageController.php
+++ app/Http/Controllers/Owner/ImageController.php
@@ -70,20 +70,16 @@ class ImageController extends Controller
         ->with(['message' => '画像登録を実施しました', 'status' => 'info']);
     }
 
-    /**
-     * Display the specified resource.
-     */
-    public function show(string $id)
-    {
-        //
-    }
 
     /**
      * Show the form for editing the specified resource.
      */
     public function edit(string $id)
     {
-        //
+        $image = Image::findOrFail($id);
+
+        return view('owner.images.edit', compact('image'));
+
     }
 
     /**
@@ -91,7 +87,17 @@ class ImageController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $request->validate([
+            'title' => ['string', 'max:50'],
+        ]);
+
+        $image= Image::findOrFail($id);
+        $image->title = $request->title;
+        $image->save();
+
+        return redirect()
+        ->route('owner.images.index')
+        ->with(['message' => '画像情報を更新しました', 'status' => 'info']);
     }
 
     /**
