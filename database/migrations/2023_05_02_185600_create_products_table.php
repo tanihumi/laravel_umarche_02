fe197e8595b55aa8befadcc016b4d8700f7ca4e7
tanihumi <x7azzsy@gmail.com>
2023-05-03 22:18:01 +0900

    product create

--- database/migrations/2023_05_02_185600_create_products_table.php
+++ database/migrations/2023_05_02_185600_create_products_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,11 @@ return new class extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('information');
+            $table->unsignedInteger('prics');
+            $table->boolean('is_selling');
+            $table->integer('sort_order')->nullable();
             $table->foreignId('shop_id')
             ->constrained()
             ->onUpdate('cascade')
@@ -20,6 +24,15 @@ return new class extends Migration
             $table->foreignId('secondary_category_id')
             ->constrained();
             $table->foreignId('image1')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image2')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image3')
+            ->nullable()
+            ->constrained('images');
+            $table->foreignId('image4')
             ->nullable()
             ->constrained('images');
             $table->timestamps();
--- resources/views/owner/shops/edit.blade.php
+++ resources/views/owner/shops/edit.blade.php
@@ -49,7 +49,7 @@
                                 <div><input type="radio" name="is_selling" value="1" class="mr-2" @if( $shop->is_selling === 1 ){ checked } @endif >販売中</div>
                                 <div><input type="radio" name="is_selling" value="0" class="mr-2" @if( $shop->is_selling === 0 ){ checked } @endif >停止中</div>
                             </div>
-                    </div>
+                        </div>
    
 
                         <div class="p-2 w-full mt-4 flex justify-around">
