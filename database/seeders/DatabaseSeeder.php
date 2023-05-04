60c948035ea9dc494e65d48496813d1e41afc20a
tanihumi <x7azzsy@gmail.com>
2023-05-04 21:09:53 +0900

    product factory

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
+use App\Models\Product;
+use App\Models\Stock;
+
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -23,5 +26,9 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
 
         ]);
+
+        Product::factory(100)->create();
+        Stock::factory(100)->create();
+
     }
 }
