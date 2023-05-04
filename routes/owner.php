c28a2f9d0b816804a2596417d0e5b8f6ddbcad67
tanihumi <x7azzsy@gmail.com>
2023-05-04 16:45:17 +0900

    route修正

--- routes/owner.php
+++ routes/owner.php
@@ -28,9 +28,9 @@ use App\Http\Controllers\Owner\ProductController;
 |
 */
 
-Route::get('/', function () {
-    return view('owner.welcome');
-});
+// Route::get('/', function () {
+//     return view('owner.welcome');
+// });
 
 
 Route::prefix('shops')->
@@ -67,10 +67,10 @@ Route::middleware('auth:owners')->group(function () {
 
 
 Route::middleware('guest')->group(function () {
-    Route::get('register', [RegisteredUserController::class, 'create'])
-                ->name('register');
+    // Route::get('register', [RegisteredUserController::class, 'create'])
+    //             ->name('register');
 
-    Route::post('register', [RegisteredUserController::class, 'store']);
+    // Route::post('register', [RegisteredUserController::class, 'store']);
 
     Route::get('login', [AuthenticatedSessionController::class, 'create'])
                 ->name('login');
