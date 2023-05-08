37a2c6cdad6fe587fd215f8776415fe3d5a8b2fa
tanihumi <x7azzsy@gmail.com>
2023-05-08 16:43:56 +0900

    show調整

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -14,7 +14,7 @@
 
                     @foreach ($products as $product)
                     <div class="w-1/4 md:p-4 p-2">
-                    <a href="">
+                    <a href="{{ route('user.items.show', ['item' => $product->id]) }}">
                         <div class="border rounded-md md:p-4 p-2">
                             <x-thumbnail filename="{{ $product->filename ?? '' }}" type="products" />
 
