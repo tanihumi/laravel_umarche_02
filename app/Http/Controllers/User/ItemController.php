ea78ed0461131a61582587f7bb5632ece894fb47
tanihumi <x7azzsy@gmail.com>
2023-05-10 20:45:36 +0900

    コンストラクタの修正

--- app/Http/Controllers/User/ItemController.php
+++ app/Http/Controllers/User/ItemController.php
@@ -8,12 +8,27 @@ use Illuminate\Http\Request;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use App\Models\Stock;
+use Closure;
 
 class ItemController extends Controller
 {
     public function __construct()
     {
         $this->middleware('auth:users');
+
+        $this->middleware(function (Request $request, Closure $next) {
+
+            $id = $request->route()->parameter('item'); //shopのid取得
+            if (!is_null($id)) { // null判定
+                $itemId = Product::availableItems()->where('products.id', $id)->exists();
+
+                if (!$itemId) { // 同じでなかったら
+                    abort(404); // 404画面表示
+                }
+            }
+
+            return $next($request);
+        });
     }
 
     public function index()
